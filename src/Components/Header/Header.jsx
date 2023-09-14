28b8af3d77fdc8d2e296bdcda170574e2bcd8811
Ypriom121 <pritomgupta121@gmail.com>
2023-09-14 20:18:03 +0600

    header part completed

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+
+import PropTypes from 'prop-types';
+
+const Header = () => {
+return (
+       <div>
+         <h2 className='mt-4 font-semibold text-4xl text-center'>Course Registration</h2>                                                                                 
+    </div>
+ );
+};
+
+Header.propTypes = {
+     Header:PropTypes.text                        
+};
+
+export default Header;
